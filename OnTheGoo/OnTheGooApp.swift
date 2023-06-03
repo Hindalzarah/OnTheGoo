98bf8097ad2bc03c5bed2ab44e9a649ac3035219
Hind <hindalzarah@outlook.com>
2023-06-04 01:07:53 +0300

    Initial Commit

---
+++ OnTheGoo/OnTheGooApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnTheGooApp.swift
+//  OnTheGoo
+//
+//  Created by Hind on 15/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct OnTheGooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
